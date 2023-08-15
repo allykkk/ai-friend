bfd7c299d3a8f5b8d0e3975902b40c05302c52de
allykkk <ally.appskk@gmail.com>
2023-08-15 20:36:49 +0300

    added margin for login

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -1,7 +1,7 @@
 // children is ReactNode
 const AuthLayout = ({ children }) => {
   return (
-    <div className="flex items-center justify-center h-full">{children}</div>
+    <div className="flex items-center justify-center h-full mt-40">{children}</div>
   );
 };
 
